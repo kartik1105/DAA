5d303406cd3e3f12d9531320d2ac566a39413334
kartik1105 <146906354+kartik1105@users.noreply.github.com>
2024-12-03 21:05:32 +0530

    Update D1.cpp

--- D1.cpp
+++ D1.cpp
@@ -1,98 +1,46 @@
-// Problem Statement No.10
-// Implement program to find all distinct subsets of a given set using Bit Masking Approach.
-// Statement: Given an array of integers arr[]. The task is to find all its subsets.
-// The subset cannot conatin ducplicate elements
-
-// Input S = {1, 2, 2}
-// Output {}, {1}, {2}, {1, 2}, {2, 2}, {1, 2, 2}
-
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
 #include <set>
+#include <algorithm>
 using namespace std;
-/*
-[10]
-[10,12]
-[10,12,12]
-[10,12] this is not allowed
-[12]
-[12,12]
-[12] this is not allowed
-*/
-void findSubsets(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ansList)
-{
-    ansList.push_back(ds);
-    for (int i = ind; i < nums.size(); i++)
-    {
-        if (i != ind && nums[i] == nums[i - 1])
-        {
-            continue;
-        }
 
-        ds.push_back(nums[i]);
-        findSubsets(i + 1, nums, ds, ansList);
-        ds.pop_back();
+// Function to find all distinct subsets
+void findSubsets(int arr[], int n) {
+    // Use a set to store subsets as strings to avoid duplicates
+    set<string> subsets;
+
+    // Loop through all possible subsets using bitmasking
+    for (int i = 0; i < (1 << n); i++) {
+        string subset = "{ ";
+        
+        // Check each bit of i
+        for (int j = 0; j < n; j++) {
+            // If the j-th bit of i is set, include arr[j] in the subset
+            if (i & (1 << j)) {
+                subset += to_string(arr[j]) + " ";
+            }
+        }
+        
+        subset += "}";
+        
+        // Add the subset to the set (duplicates will be automatically removed)
+        subsets.insert(subset);
+    }
+
+    // Print all distinct subsets
+    for (auto subset : subsets) {
+        cout << subset << endl;
     }
 }
 
-vector<vector<int>> AllSubsets(int arr[], int n)
-{
-    vector<int> nums(arr, arr + n);
-    vector<int> ds;
-    sort(nums.begin(), nums.end());
-    vector<vector<int>> ansList;
-    findSubsets(0, nums, ds, ansList);
-    return ansList;
-}
-void BitSubsets(int arr[],int n)
-{
-    set<vector<int>> ans;
-    int cnt = (1 << n);
-    for(int i =0;i<cnt;i++)
-    {
-        vector<int> temp;
-        for(int j =0;j<n;j++)
-        {
-            if(i&(1<<j))
-            {
-                temp.push_back(arr[j]);
-            }
-        }
-        ans.insert(temp);
-    }   
-    for (auto subset : ans)
-    {
-        cout << "[";
-        for (int i = 0; i < subset.size(); i++)
-        {
-            cout << subset[i];
-            if (i < subset.size() - 1)
-            {
-                cout << ", ";
-            }
-        }
-        cout << "], "<<endl;
-    }
-}
-int main()
-{
-    int set[] = {10, 12, 14};
-    // vector<vector<int>> subsets = AllSubsets(set, 3);
-    // for (auto subset : subsets)
-    // {
-    //     cout << "[";
-    //     for (int i = 0; i < subset.size(); i++)
-    //     {
-    //         cout << subset[i];
-    //         if (i < subset.size() - 1)
-    //         {
-    //             cout << ", ";
-    //         }
-    //     }
-    //     cout << "], "<<endl;
-    // }
-    BitSubsets(set,3);
+int main() {
+    int arr[] = {1, 2, 2};  // Input set
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Sort the array to ensure subsets are generated in lexicographical order
+    sort(arr, arr + n);
+
+    // Find and print all distinct subsets
+    findSubsets(arr, n);
+
     return 0;
 }
