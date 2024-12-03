0b6fa8b244b49be0eef55c6f6e1acad538e6d595
kartik1105 <146906354+kartik1105@users.noreply.github.com>
2024-12-03 20:32:13 +0530

    Update A3.cpp

--- A3.cpp
+++ A3.cpp
@@ -1,43 +1,49 @@
+// Problem Statement No.03
+// Implement a problem of smallest number with at least n trailing 0s in factorial.
+// Given a number n. The task is to find the smallest number whose factorial contains at least n trailing 0s.
+// Input: n=1       Output: 5
+// Input: n=6       Output: 25
+
 #include <iostream>
 using namespace std;
 
-// Function to count the number of trailing zeroes in factorial of x
-int countTrailingZeroes(int x) {
-    int count = 0;
-    while (x >= 5) {
-        count += x / 5;
-        x /= 5;
-    }
-    return count;
-}
+// Function to find the smallest number whose factorial has at least 'n' trailing zeroes
+int findNum(int n)
+{
+    int num = 1; // Start from 1
+    int cnt = 0; // Variable to count the number of trailing zeroes
 
-// Function to find the smallest number whose factorial has at least n trailing zeroes
-int findSmallestNumberWithNTrailingZeroes(int n) {
-    int left = 0, right = 5 * n; // An upper bound for the search
-    int result = -1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        int zeroes = countTrailingZeroes(mid);
-        
-        if (zeroes >= n) {
-            result = mid;
-            right = mid - 1; // We try to find a smaller number
-        } else {
-            left = mid + 1; // We need to try a larger number
+    // Infinite loop to check each number's factorial for trailing zeroes
+    while (true)
+    {
+        int temp = num; // Temporary variable to hold the current number
+        // Count how many times the number is divisible by 5 (this determines the number of trailing zeroes)
+        while (temp % 5 == 0)
+        {
+            cnt++; // Increment the count of trailing zeroes
+            temp /= 5; // Divide by 5 to check the next factor of 5
         }
+        
+        // If the number of trailing zeroes is greater than or equal to 'n', return the current number
+        if (cnt >= n)
+        {
+            return num;
+        }
+        
+        num++; // Increment the number to check the next number
     }
-    
-    return result;
 }
 
-int main() {
+int main()
+{
     int n;
-    cout << "Enter the number of trailing zeroes: ";
+    // Prompt the user to enter the number of trailing zeroes
+    cout << "Enter the number of trailing zeroes" << endl;
     cin >> n;
 
-    int result = findSmallestNumberWithNTrailingZeroes(n);
-    cout << "The smallest number whose factorial has at least " << n << " trailing zeroes is: " << result << endl;
-
+    // Call the function and display the result
+    cout << findNum(n) << " is the smallest number having " << n << " number of trailing zeroes" << endl;
+    
     return 0;
 }
+
